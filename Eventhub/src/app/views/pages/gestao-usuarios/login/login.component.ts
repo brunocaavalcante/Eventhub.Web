0756074c9b36888594ef2c33f99309ce3e35d88f
Bruno Cavalcante <brunocsilva@policiamilitar.sp.gov.br>
2025-09-23 10:40:32 -0300

    Template tela de login

---
+++ Eventhub/src/app/views/pages/gestao-usuarios/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
