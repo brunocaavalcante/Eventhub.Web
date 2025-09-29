0e0eeaf59ccb402e277058e35f2c504749fd7b26
Bruno Cavalcante <brunocsilva@policiamilitar.sp.gov.br>
2025-09-28 23:54:09 -0300

    Component meus eventos, template sem eventos cadastrados

---
+++ Eventhub/src/app/views/pages/gestao-eventos/meus-eventos/meus-eventos.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { BaseComponent } from '../../../../core/components/base.component';
+import { CommonModule } from '@angular/common';
+import { MatButton } from "@angular/material/button";
+
+@Component({
+  selector: 'app-meus-eventos',
+  imports: [CommonModule, MatButton],
+  templateUrl: './meus-eventos.component.html',
+  styleUrl: './meus-eventos.component.scss'
+})
+export class MeusEventosComponent extends BaseComponent {
+  meusEventos = [];
+
+}
