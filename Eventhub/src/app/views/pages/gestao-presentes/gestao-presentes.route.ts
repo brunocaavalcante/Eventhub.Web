980aed48efa8bf546c6affdc6fc88bac7a6b557d
Bruno Cavalcante <brunocavalcantedsilva@gmail.com>
2025-12-29 23:38:53 -0300

    #346 CONFIGURAÇÃO DE ROTAS E NAVEGAÇÃO PARA EDIÇÃO DE PRESENTES

--- Eventhub/src/app/views/pages/gestao-presentes/gestao-presentes.route.ts
+++ Eventhub/src/app/views/pages/gestao-presentes/gestao-presentes.route.ts
@@ -1,8 +1,12 @@
 import { authGuard } from "../../../core/utils/guards/auth.guard";
 import { CadastrarPresentesComponent } from "./cadastrar-presentes/cadastrar-presentes.component";
 import { ConsultarPresentesComponent } from "./consultar-presentes/consultar-presentes.component";
+import { EditarPresentesComponent } from "./editar-presentes/editar-presentes.component";
+import { PagamentoPresenteComponent } from "./pagamento-presente/pagamento-presente.component";
 
 export const routes = [
     { path: ':idEvento', component: ConsultarPresentesComponent, canActivate: [authGuard] },
     { path: 'cadastrar/:idEvento', component: CadastrarPresentesComponent, canActivate: [authGuard] },
+    { path: 'pagar/:id', component: PagamentoPresenteComponent, canActivate: [authGuard] },
+    { path: 'editar/:idEvento/:id', component: EditarPresentesComponent, canActivate: [authGuard] }
 ];
