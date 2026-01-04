4c7ad4b51662abc7d930ea0de0064674d99e0496
Bruno Cavalcante <brunocavalcantedsilva@gmail.com>
2026-01-03 23:24:27 -0300

    Correção warning do import scss

--- Eventhub/src/app/app.component.ts
+++ Eventhub/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NgxUiLoaderModule } from 'ngx-ui-loader';
 import { LayoutComponent } from "./views/base/layout/layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutComponent, NgxUiLoaderModule],
+  imports: [ LayoutComponent, NgxUiLoaderModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true
